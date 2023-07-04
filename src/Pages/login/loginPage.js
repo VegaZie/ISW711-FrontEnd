986af40bbb76801c6e80bbacbfa0cedac01960f4
VegaZie <davidgerardovegas29@gmail.com>
2023-07-04 14:06:28 -0600

    Implementación de boton de registro en el loginPage

--- src/Pages/login/loginPage.js
+++ src/Pages/login/loginPage.js
@@ -1,12 +1,16 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-import FormInput from '../../components/formInput/formInput';
-import Button from '../../components/button/button';
-import './loginPage.scss';
+import FormInput from "../../components/formInput/formInput";
+import Button from "../../components/button/button";
+import "./loginPage.scss";
 
 const LoginPage = () => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleRegister = () => {
+    //enviar a página de registro
+  };
 
   const handleLogin = () => {
     // axios.post('process.env.SESSION_API_URL', { email, password })
@@ -22,27 +26,30 @@ const LoginPage = () => {
 
   return (
     <div className="login-page">
-    <div className="login-container">
-      <h2>Login</h2>
-      <form>
-        <FormInput
-          type="email"
-          label="Email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          required
-        />
-        <FormInput
-          type="password"
-          label="Password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          required
-        />
-        <Button onClick={handleLogin}>Login</Button>
-      </form>
+      <div className="login-container">
+        <h2>Login</h2>
+        <form>
+          <FormInput
+            type="email"
+            label="Email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
+          <FormInput
+            type="password"
+            label="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+          <Button onClick={handleLogin}>Login</Button>
+        </form>
+        <p className="register-link" onClick={handleRegister}>
+          ¿No tienes una cuenta? Crear una cuenta
+        </p>
+      </div>
     </div>
-  </div>
   );
 };
 
