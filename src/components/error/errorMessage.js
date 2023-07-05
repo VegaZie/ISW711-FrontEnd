1240d94f814d40b3c05d81ebacdd132041315464
VegaZie <davidgerardovegas29@gmail.com>
2023-07-05 10:35:20 -0600

    elemento errorMessage actualizado

--- src/components/error/errorMessage.js
+++ src/components/error/errorMessage.js
@@ -1,13 +1,23 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import './errorMessage.scss';
 
-const ErrorMessage = ({ title, message }) => {
+const ErrorMessage = ({ message, onAccept }) => {
   return (
-    <div className="error-message">
-      <h2>{title}</h2>
-      <p>{message}</p>
+    <div className="error-message-overlay">
+      <div className="error-message">
+        <p className="message">{message}</p>
+        <button className="accept-button" onClick={onAccept}>
+          Aceptar
+        </button>
+      </div>
     </div>
   );
+};
+
+ErrorMessage.propTypes = {
+  message: PropTypes.string.isRequired,
+  onAccept: PropTypes.func.isRequired,
 };
 
 export default ErrorMessage;
