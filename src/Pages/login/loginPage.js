ef7eb04644a45a7a5d860e90f922e6947ac01365
VegaZie <davidgerardovegas29@gmail.com>
2023-07-06 10:26:02 -0600

    login funcional, falta validar que el usuario este verificado

--- src/Pages/login/loginPage.js
+++ src/Pages/login/loginPage.js
@@ -1,27 +1,42 @@
 import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
 
 import FormInput from "../../components/formInput/formInput";
 import Button from "../../components/button/button";
+import ErrorMessage from "../../components/error/errorMessage";
 import "./loginPage.scss";
 
 const LoginPage = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [error, setError] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("");
+  const navigate = useNavigate();
 
   const handleRegister = () => {
-    //enviar a página de registro
+    navigate("/register");
   };
 
-  const handleLogin = () => {
-    // axios.post('process.env.SESSION_API_URL', { email, password })
-    //   .then(response => {
-    //     const token = response.data.token;
-    //     // Guardar el token en el sessionStorage
-    //     // Redireccionar a la página de inicio.
-    //   })
-    //   .catch(error => {
-    //     // Manejar el error de autenticación.
-    //   });
+  const handleLogin = (event) => {
+    event.preventDefault();
+
+    axios
+      .post(process.env.REACT_APP_SESSION_AUTHETICATE, { email: email, password: password })
+      .then((response) => {
+        console.log(response.data.token);
+        sessionStorage.setItem('token', response.data.token);
+        // navigate("/home")
+      })
+      .catch((errorM) => {
+        console.log(errorM);
+        setErrorMessage(errorM);
+        setError(true);
+      });
+  };
+
+  const handleAcceptError = () => {
+    setError(false);
   };
 
   return (
@@ -49,6 +64,12 @@ const LoginPage = () => {
           Don't have an account? Create an account
         </p>
       </div>
+      {error && (
+          <ErrorMessage
+            message={"Registro de usuario fallido " + errorMessage}
+            onAccept={handleAcceptError}
+          />
+        )}
     </div>
   );
 };
