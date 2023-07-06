6fae07a8a73d5f43891336570abd48bbd1ce96ce
VegaZie <davidgerardovegas29@gmail.com>
2023-07-06 14:29:39 -0600

    Añadido del homepage a las rutas

--- src/Routes/index.js
+++ src/Routes/index.js
@@ -2,21 +2,31 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Login from "../Pages/login/loginPage";
 import Register from "../Pages/registration/registrationPage";
+import HomePage from "../Pages/home/homePage";
 import Error from "../Pages/error/errorPage";
 
 const Router = () => {
-    return (
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Login />} errorElement={<Error/>}/>
-          <Route path="/register" element={<Register />} errorElement={<Error/>}/>
-        </Routes>
-      </BrowserRouter>
-    );
-  };
-  
-  export default Router;
-  /*  <BrowserRouter>
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login />} errorElement={<Error />} />
+        <Route
+          path="/register"
+          element={<Register />}
+          errorElement={<Error />}
+        />
+        <Route
+          path="/home"
+          element={<HomePage />}
+          errorElement={<Error />}
+        ></Route>
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default Router;
+/*  <BrowserRouter>
         <Routes>
           <Route path="/" element={<Login />} errorElement={<Error/>}/>
           <Route path="/home" element={<Home />} errorElement={<Error/>}>
