e5bba47411bd6a883d9a78552d9ae0b9d44616ea
VegaZie <davidgerardovegas29@gmail.com>
2023-07-24 15:52:17 -0600

    Actualización del homepage con información dinamica

--- src/Pages/home/homePage.js
+++ src/Pages/home/homePage.js
@@ -1,19 +1,74 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
+import axios from "axios";
 
 import Navigation from "../../components/navigation/navigationBar";
 import Card from "../../components/card/card";
+import Spinner from "../../components/spinner/spinner";
+import ErrorMessage from "../../components/error/errorMessage";
 import "./homePage.scss";
 
 const HomePage = () => {
+  // Declaración del estado dataList usando useState
+  const [dataList, setDataList] = useState([]);
+
+  // Obtebenos el token, nombre, rol y userID del sessionStorage
+  const token = sessionStorage.getItem("token");
   const name = sessionStorage.getItem("name");
   const role = sessionStorage.getItem("role");
+  const userID = sessionStorage.getItem("id");
+
+  // Convertimos el rol a un valor booleano para userRole
+  const userRole = role === "admin";
+
+  //Manejo de errores
+  const [error, setError] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("");
+
+  const handleAcceptError = () => {
+    setError(false);
+  };
+
   const navigate = useNavigate();
+
   const handleLogOut = () => {
     sessionStorage.clear();
     navigate("/");
   };
 
+  useEffect(() => {
+    if (userRole) {
+      axios
+        .get(process.env.REACT_APP_USER, {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        })
+        .then(function (response) {
+          // handle success
+          setDataList(response.data);
+        })
+        .catch(function (error) {
+          setErrorMessage("Algo a salido mal " + error);
+          setError(true);
+        });
+    } else {
+      axios
+        .get(process.env.REACT_APP_PROMTS + `?userID=${userID}`, {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        })
+        .then(function (response) {
+          // handle success
+          setDataList(response.data);
+        })
+        .catch(function (error) {
+          setErrorMessage("Algo a salido mal " + error);
+          setError(true);
+        });
+    }
+  }, [userID, userRole, token]);
   return (
     <div className="home-page">
       <Navigation
@@ -22,27 +77,47 @@ const HomePage = () => {
         role={role}
         onLogout={handleLogOut}
       />
-      <div className="home-page__content">
-        <h2>Elementos</h2>
-        <div className="home-page__card-container">
-          <div className="home-page__card-item">
-            <Card
-              name="Elemento 1"
-              type="Tipo 1"
-              tags="Tag 1, Tag 2, Tag 3"
-              isAdmin={false}
-            />
-          </div>
-          <div className="home-page__card-item">
-            <Card
-              name="Elemento 2"
-              email="prueba@email.com"
-              status="active"
-              isAdmin={true}
-            />
+      {dataList.length > 0 ? ( // Verificar si dataList tiene datos
+        <div className="home-page__content">
+          <h2>Elementos</h2>
+          <div className="home-page__card-container">
+            {userRole ? (
+              <div className="home-page__card-item">
+                {dataList.map((item, index) => (
+                  <Card
+                    key={index}
+                    id={item._id}
+                    name={item.name}
+                    email={item.email}
+                    status={item.verified}
+                    isAdmin={userRole}
+                  />
+                ))}
+              </div>
+            ) : (
+              <div className="home-page__card-item">
+                {dataList.map((item, index) => (
+                  <Card
+                    key={item._id}
+                    id={item._id}
+                    name={item.name}
+                    type={item.type}
+                    tags={item.tags}
+                    isAdmin={userRole}
+                    data={item}
+                  />
+                ))}
+              </div>
+            )}
           </div>
         </div>
-      </div>
+      ) : (
+        <Spinner />
+      )}
+
+      {error && (
+        <ErrorMessage message={errorMessage} onAccept={handleAcceptError} />
+      )}
     </div>
   );
 };
