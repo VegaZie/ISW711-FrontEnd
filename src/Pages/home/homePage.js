ac99f93bf5ef7471bffd89d3a5a5fd8341f89c86
VegaZie <davidgerardovegas29@gmail.com>
2023-07-26 18:19:28 -0600

    se implementaron todas la ventas de los distintos cruds

--- src/Pages/home/homePage.js
+++ src/Pages/home/homePage.js
@@ -3,11 +3,13 @@ import { useNavigate } from "react-router-dom";
 import axios from "axios";
 
 import Navigation from "../../components/navigation/navigationBar";
+import Button from "../../components/button/button";
 import Card from "../../components/card/card";
 import Spinner from "../../components/spinner/spinner";
 import ErrorMessage from "../../components/error/errorMessage";
 import NoPromptsMessage from "../../components/noPromtsMessage/noPromtMessage";
 import RegistrationPopup from "../../components/popup/registrationUserPopup";
+import PromtCreatePopup from "../../components/popup/promtCreatePromtPopup";
 import "./homePage.scss";
 
 const HomePage = () => {
@@ -17,8 +19,9 @@ const HomePage = () => {
   // Estado para mostrar el mensaje de "No tienes promts registrados"
   const [showNoPromptsMessage, setShowNoPromptsMessage] = useState(false);
 
-  // Estado para mostrar el mensaje de "No tienes promts registrados"
+  // Estado para mostrar los registros de usuario y promts
   const [showRegistrarionPopup, setShowRegistrarionPopup] = useState(false);
+  const [showPromtPopup, setShowPromtPopup] = useState(false);
 
   // Obtebenos el token, nombre, rol y userID del sessionStorage
   const token = sessionStorage.getItem("token");
@@ -49,6 +52,7 @@ const HomePage = () => {
 
   const onClosePopup = () => {
     setShowRegistrarionPopup(false);
+    setShowPromtPopup(false);
   };
   // Función para obtener los promts o usuarios según el rol
   const fetchData = useCallback(() => {
@@ -89,7 +93,7 @@ const HomePage = () => {
     if (userRole) {
       setShowRegistrarionPopup(true);
     } else {
-      // Redirigir a la página de creación de promts
+      setShowPromtPopup(true);
     }
   };
 
@@ -106,16 +110,12 @@ const HomePage = () => {
       {userRole ? (
         <div className="home-page__content">
           <h2>Lista de Usuarios</h2>
-          <button onClick={handleAddClick} className="home-page__add-button">
-            Agregar Usuario
-          </button>
+          <Button onClick={handleAddClick}>Agregar Usuario</Button>
         </div>
       ) : (
         <div className="home-page__content">
           <h2>Lista de Promts</h2>
-          <button onClick={handleAddClick} className="home-page__add-button">
-            Agregar Promt
-          </button>
+          <Button onClick={handleAddClick}>Agregar Promt</Button>
         </div>
       )}
 
@@ -127,13 +127,25 @@ const HomePage = () => {
             {userRole ? (
               <div className="home-page__card-item">
                 {dataList.map((item) => (
-                  <Card key={item._id} isAdmin={userRole} data={item} />
+                  <Card
+                    key={item._id}
+                    isAdmin={userRole}
+                    data={item}
+                    token={token}
+                    onSucess={fetchData}
+                  />
                 ))}
               </div>
             ) : (
               <div className="home-page__card-item">
                 {dataList.map((item) => (
-                  <Card key={item._id} isAdmin={userRole} data={item} />
+                  <Card
+                    key={item._id}
+                    isAdmin={userRole}
+                    data={item}
+                    token={token}
+                    onSucess={fetchData}
+                  />
                 ))}
               </div>
             )}
@@ -154,6 +166,14 @@ const HomePage = () => {
 
       {showRegistrarionPopup && (
         <RegistrationPopup onSucess={fetchData} onClose={onClosePopup} />
+      )}
+      {showPromtPopup && (
+        <PromtCreatePopup
+          onSucess={fetchData}
+          onClose={onClosePopup}
+          id={userID}
+          token={token}
+        />
       )}
     </div>
   );
