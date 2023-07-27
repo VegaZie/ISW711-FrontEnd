6f0ab87e17c3d626a760596caa138465d08555a6
VegaZie <davidgerardovegas29@gmail.com>
2023-07-26 19:50:41 -0600

    Ventana de edición de usuario terminada

--- src/components/popup/userPopup.js
+++ src/components/popup/userPopup.js
@@ -1,18 +1,72 @@
 import React, { useState } from "react";
-//import Button from "../button/button";
+import axios from "axios";
+
+import ErrorMessage from "../../components/error/errorMessage";
+import SuccessMessage from "../../components/successMessage/successMessage";
+import Button from "../button/button";
 
 import "./popup.scss";
 
-const UserEditPopup = ({ data, onClose }) => {
+const UserEditPopup = ({ data, onClose, token, onSucess }) => {
+  const userID = data._id;
   const [editedName, setEditedName] = useState(data.name);
   const [editedEmail, setEditedEmail] = useState(data.email);
   const [editedVerified, setEditedVerified] = useState(data.verified);
 
-  const handleEdit = () => {};
+  const [error, setError] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("");
+  const [success, setSuccess] = useState(false);
+  const [successMessage, setSuccessMessage] = useState("");
 
-  const handleDelete = () => {};
+  const handleEdit = () => {
+    const data = {
+      name: editedName,
+      email: editedEmail,
+      verified: editedVerified,
+    };
+
+    axios
+      .patch(process.env.REACT_APP_USER + `?id=${userID}`, data, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      })
+      .then((response) => {
+        setSuccess(true);
+        setSuccessMessage("Actualización de usuario exitosa");
+      })
+      .catch((error) => {
+        console.log(error);
+        setErrorMessage("Actualización del promt fallido. " + error);
+        setError(true);
+      });
+  };
+
+  const handleDelete = () => {
+    axios
+      .delete(process.env.REACT_APP_USER + `?id=${userID}`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      })
+      .then((response) => {
+        setSuccess(true);
+        setSuccessMessage("Eliminación de usuario exitosa");
+      })
+      .catch((errorM) => {
+        console.log(errorM);
+        setErrorMessage("Eliminación del promt fallida.");
+        setError(true);
+      });
+  };
 
   const handleCancel = () => {
+    onClose();
+  };
+
+  const handleAcceptSuccess = () => {
+    setSuccess(false);
+    onSucess();
     onClose();
   };
 
@@ -41,11 +95,20 @@ const UserEditPopup = ({ data, onClose }) => {
           <option value={false}>No verificado</option>
         </select>
         <div className="popup__buttons">
-          <button onClick={handleEdit}>Editar</button>
-          <button onClick={handleDelete}>Eliminar</button>
-          <button onClick={handleCancel}>Salir</button>
+          <Button onClick={handleEdit}>Editar</Button>
+          <Button onClick={handleDelete}>Eliminar</Button>
+          <Button onClick={handleCancel}>Salir</Button>
         </div>
       </div>
+      {success && (
+        <SuccessMessage
+          message={successMessage}
+          onAccept={handleAcceptSuccess}
+        />
+      )}
+      {error && (
+        <ErrorMessage message={errorMessage} onAccept={handleAcceptError} />
+      )}
     </div>
   );
 };
